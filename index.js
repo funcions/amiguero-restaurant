85bf421c6d9fec5bb5e044324913d513d87c65b0
funcions <mijailwwe@gmail.com>
2023-12-07 17:46:20 -0500

    Descripción del cambio: gaa

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = required("express")
+const cors = required ("cors")
+
+const app = express();
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+app.use(cors())
+
+app.listen(3000, (err)) => {
+    if(err) console.log("an error ocurred")
+    console.log("server running on port 3000")
+}
